6360dae8429c554e2a48bec335a2141daccc46e8
Yoshi Y <95995009+yoshiyyc@users.noreply.github.com>
2022-12-21 16:22:58 +0800

    Delete account-friends-management.js

--- js/account-friends-management.js
+++
@@ -1,3 +0,0 @@
-// Popover
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
